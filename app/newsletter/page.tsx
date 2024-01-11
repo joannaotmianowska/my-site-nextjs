fe4c9f30062f17bdf58c430d879816054343f9ad
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-01-11 21:33:33 +0100

    show list of newsletters id

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,12 +1,28 @@
 import { getAllNewslettersSlugs } from "@/lib/newsletters";
+import Link from 'next/link';
 
+export default function Newsletter() {
+  const newsletters = getAllNewslettersSlugs();
 
-export default function Newsletter(props: any) {
-  console.log('params', props);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        newsletter
+        <div>
+          <ul>
+            {newsletters.map((newsletter: any) => {
+              console.log('newsletter', newsletter)
+              return (
+              <li key={newsletter.params.id}>
+                <Link 
+                  href={`/newsletter/${newsletter.params.id}`}
+                  className='font-mono hover:font-bold hover:underline'
+                >
+                  {newsletter.params.id}
+                </Link>
+              </li>
+            )})}
+          </ul>
+      </div>
       </div>
     </main>
   );
--- lib/newsletters.ts
+++ lib/newsletters.ts
@@ -4,36 +4,6 @@ import matter from 'gray-matter';
 
 const newsletterDirectory = path.join(process.cwd(), 'newsletter-episodes');
 
-export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(newsletterDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
-
-    // Read markdown file as string
-    const fullPath = path.join(newsletterDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-    };
-  });
-  // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-}
-
 export function getNewslettersData(slug: string) {
   const fullPath = path.join(newsletterDirectory, `${slug}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
