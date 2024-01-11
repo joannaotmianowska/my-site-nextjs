e5664638ca3fe9137ac1aef471774d91db1e44ce
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-01-11 15:19:19 +0100

    commit

--- app/newsletter/[slug]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-    return <div>nie znaleziono newslettera. Zajrzyj do spisu wszystkich odcinków.</div>;
-};
-
-export default NotFound;
--- app/podcast/[slug]/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-    return <div>Nie znaleziono odcinka. Zajrzyj do listy odcinków podcastu</div>;
-};
-
-export default NotFound;
