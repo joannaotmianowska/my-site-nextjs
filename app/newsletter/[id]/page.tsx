5d1258bcb7b0d3c6c21eec04c700beb5dec1a5e3
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-01-18 12:34:40 +0100

    form for mailerlite added

--- app/newsletter/[id]/page.tsx
+++ app/newsletter/[id]/page.tsx
@@ -4,13 +4,30 @@ export default async function Newsletter({ params }: any) {
   const data = await getNewslettersData(params.id);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+      <div className='flex flex-row z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <div>
           <h1 className='text-4xl font-bold font-mono text-center'>{data.title}</h1>
           <h2 className='text-1xl font-mono text-center'>Oryginalna data wysyłki: {data.date}</h2>
           <div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
+        </div>
       </div>
-      </div>
+      <div className="ml-form-embed"
+          data-account="1266630:w2b1g4j9n7"
+          data-form="5962655:x7z2u3">
+        </div>
+      <div dangerouslySetInnerHTML={{ __html: `
+            <!-- MailerLite Universal -->
+            <script>
+            (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
+            var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+            f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+            var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
+            _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+            
+            var ml_account = ml('accounts', '1266630', 'w2b1g4j9n7', 'load');
+            </script>
+            <!-- End MailerLite Universal -->
+          ` }} />
   </main>
   );
 }
