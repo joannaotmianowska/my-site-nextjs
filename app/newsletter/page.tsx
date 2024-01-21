f588e578dfa9818e89218c43e8137d8e6ea9186f
joannaotmianowska <otmianowska.joanna@gmail.com>
2024-01-21 21:56:15 +0100

    draft styles for keywords on newsletters list

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -8,19 +8,19 @@ export default async function Newsletter() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex'>
         <div>
-          <h1 className='text-4xl font-bold'>Archiwalne newslettery</h1>
+          <h1 className='text-4xl font-bold mb-8 text-center'>Archiwalne newslettery</h1>
           <ul>
             {newsletters.map(async (newsletter: any) => {
               const data = await getNewslettersData(newsletter.params.id);
               return (
-                <li key={newsletter.params.id}>
+                <li key={newsletter.params.id} className='mb-5'>
                   <Link
                     href={`/newsletter/${newsletter.params.id}`}
                     className='hover:font-bold underline'
                   >
-                    <h3>{data.title}</h3>
+                    <h3 className='text-xl font-bold p-1 m-1'>{data.title}</h3>
                   </Link>
-                  <p>{data.keywords.map((topic: string) => <span className='border border-sky-400 rounded-lg bg-sky-400 p-0.5 m-0.5' key={topic}>{topic} </span>)}</p>
+                  <p>{data.keywords.map((topic: string) => <span className='border border-violet-300 rounded-lg bg-violet-300 p-1 m-1 shadow-sm' key={topic}>{topic} </span>)}</p>
                 </li>
               );
             })}
